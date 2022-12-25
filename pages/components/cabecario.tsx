9df286b00792fffb6e09b84fa4e1fe58b820209a
CARTUCHO SN <85890520+Guilherme-Ferreira-Tombini@users.noreply.github.com>
2022-12-25 18:25:42 +0000

    Aplicado paginação no site

--- pages/components/cabecario.tsx
+++ pages/components/cabecario.tsx
@@ -1,15 +1,24 @@
+import Link from 'next/link'
 import styles from '../../styles/Cabecario.module.css'
 
 export default function Cabecario() {
   return (
     <div className={styles.cabecario}>
       <div className={styles.titulo}>
-        <h1>Portifolio Pessoal</h1>
+        <h1><Link href="/" className={styles.pagina}>
+          <a>Portfolio Pessoal</a>
+        </Link></h1>
       </div>
       <div className={styles.menu}>
-        <h2>Sobre mim</h2>
-        <h2>Projetos</h2>
-        <h2>Habilidades</h2>
+        <h2><Link href="../pagina2" className={styles.pagina}>
+          <a>Sobre mim</a>
+        </Link></h2>
+        <h2><Link href="../pagina3" className={styles.pagina}>
+          <a>Projetos</a>
+        </Link></h2>
+        <h2><Link href="../pagina4" className={styles.pagina}>
+          <a>Habilidades</a>
+        </Link></h2>
       </div>
     </div>
   )
---
+++ pages/pagina3.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/Home.module.css'
+import Header from './components/cabecario';
+import Card from './components/card1';
+import Footer from './components/footer'
+
+export default function Pagina3() {
+  return (
+    <>
+      <div className={styles.fundo}>
+        <div className={styles.container}>
+          <Header />
+
+          <main className={styles.main}>
+
+          </main>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
