8b93056c1a23f498e64e4b5655d89d14c41f5499
CARTUCHO SN <85890520+Guilherme-Ferreira-Tombini@users.noreply.github.com>
2022-12-25 14:41:53 +0000

    Estilizado a pagina inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import styles from '../styles/Home.module.css'
 import Header from './components/cabecario';
 import Card from './components/card1';
+import Footer from './components/footer'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Header />
+    <>
+      <div className={styles.fundo}>
+        <div className={styles.container}>
+          <Header />
 
-      <main className={styles.main}>
-        <Card />
-      </main>
-    </div>
+          <main className={styles.main}>
+            <Card />
+          </main>
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
