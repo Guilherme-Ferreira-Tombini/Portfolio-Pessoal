e258cf21afca004a75e50767f18dec5dcbe142a2
CARTUCHO SN <85890520+Guilherme-Ferreira-Tombini@users.noreply.github.com>
2022-12-26 01:30:48 +0000

    Estilização do footer - parte 1

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -2,9 +2,29 @@ import styles from '../../styles/Footer.module.css'
 
 export default function Footer() {
   return (
-    <div>
-      <footer className={styles.footer}>
-      </footer>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.interno}>
+
+        <div className={styles.logo}>
+          <div className={styles.titulo}>PP</div>
+          <div className={styles.titulo2}>Portfolio Pessoal</div>
+
+        </div>
+
+        <div className={styles.contatos}>
+          <div>Contato</div>
+          <div className={styles.icons}>
+            <a className={styles.imge} href="https://www.linkedin.com/in/guilherme-ferreira-tombini-593015247/"><img src="/linkedin.png"></img></a>
+            <a className={styles.imge} href=""><img src="/discord.png"></img></a>
+            <a className={styles.imge} href=""><img src="/email.png"></img></a>
+            <a className={styles.imge} href=""><img src="/instagram.png"></img></a>
+          </div>
+        </div>
+
+        <div className={styles.texto}>
+          <div className={styles.cop}>© 2022 Guilherme Ferreira Tombini</div>
+        </div>
+      </div>
+    </footer>
   )
 }
