4dadcf6dea93cc7dd639b0dff60a4c36a0a0c5fc
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-12-29 18:38:29 -0300

    Adicionado menu para mobile - parte 1

--- pages/components/cabecario.tsx
+++ pages/components/cabecario.tsx
@@ -4,7 +4,33 @@ import styles from '../../styles/Cabecario.module.css'
 
 
 export default function Cabecario() {
+
+  const aparecer = () => {
+    let btn = document.querySelector('#button') as HTMLLIElement
+   
+    btn.onclick = function () {
+      let card = document.querySelector('#card') as HTMLLIElement
+        if (card.style.display == 'none') card.style.display = 'block';
+        else card.style.display = 'none';
+    }
+  };
+
+
   return (
+    <>
+    <div id="card" className={styles.card}>
+      <div className={styles.menu2}>
+        <h2><Link href="../pagina2" className={styles.pagina}>
+          <a>Sobre mim</a>
+        </Link></h2>
+        <h2><Link href="../pagina3" className={styles.pagina}>
+          <a>Projetos</a>
+        </Link></h2>
+        <h2><Link href="../pagina4" className={styles.pagina}>
+          <a>Habilidades</a>
+        </Link></h2>
+      </div>
+    </div>
     <div className={styles.cabecario}>
       <div className={styles.titulo}>
         <h1><Link href="/" className={styles.pagina}>
@@ -25,22 +51,9 @@ export default function Cabecario() {
       </div>
 
       <div className={styles.icon}>
-       <button className={styles.botao}><Image src="/icone.png" width="40px" height="40px"/></button> 
+       <button onClick={() => aparecer()} id="button" className={styles.botao}><Image id="img" src="/icone.png" width="40px" height="40px"/></button> 
       </div>
-
-      <div className={styles.card}>
-      <div className={styles.menu2}>
-        <h2><Link href="../pagina2" className={styles.pagina}>
-          <a>Sobre mim</a>
-        </Link></h2>
-        <h2><Link href="../pagina3" className={styles.pagina}>
-          <a>Projetos</a>
-        </Link></h2>
-        <h2><Link href="../pagina4" className={styles.pagina}>
-          <a>Habilidades</a>
-        </Link></h2>
       </div>
-    </div>
-      </div>
+      </>
   )
 }
